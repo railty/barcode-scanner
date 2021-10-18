bcbff97cbf89b0dee9e2643c6b1e840b26e73221
swn ning <zxning@gmail.com>
2021-10-18 11:47:10 -0400

    update

--- App.js
+++ App.js
@@ -89,6 +89,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10
   },
   barcodebox: {
     backgroundColor: 'tomato',
