9c3e1a981cc7c6eeec11ca1149ad07353ec3e5ac
shawn ning <zxning@gmail.com>
2021-10-18 09:23:30 -0400

    update

--- App.js
+++ App.js
@@ -1,11 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Clipboard, Dimentions, StyleSheet, TextInput, Text, View, Button } from 'react-native';
+import {Constants, BarCodeScanner} from "expo-barcode-scanner";
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function App() {
+  const [cameraPermission, setCameraPermission] = useState("unknown");
+  const [text, setText] = useState("not yet scanned");
+  const [camera, setCamera] = useState("front");
+
+  const [code, setCode] = useState({type:'', data:''});
+  
+  const askForCameraPermission = () => {
+    askForCameraPermissionAsync();
+  }
+
+  const askForCameraPermissionAsync = async() => {
+    console.log("requesting");
+    let rc = await BarCodeScanner.requestPermissionsAsync()
+    setCameraPermission(rc.status)
+  }
+
+  useEffect((x)=>{
+    askForCameraPermission();
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+  });
+
+  const barcodeScanned = ({ type, data }) => {
+    console.log("barcode scaned");
+    console.log("type=", type);
+    console.log("data=", data);
+    setCode({type, data});
+  }
+
+  if (cameraPermission == "unknown" || cameraPermission == "denied"){
+    return (
+      <View style={styles.container}>
+        <Text>{cameraPermission}</Text>
+        <Button title="Allow Camera" onPress={askForCameraPermission} />
+      </View>
+    );
+  
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.barcodebox}>
+          <BarCodeScanner type={camera} style={{width:400, height:300}} onBarCodeScanned={barcodeScanned} />
+      </View>
+
+      <View style={{ flexDirection: 'row' }}>
+        <View style={{ padding: 10 }}>
+          <Button title={camera} onPress={()=>{
+            if (camera == "front") setCamera("back");
+            else setCamera("front");
+          }} />
+        </View>
+        <View style={{ padding: 10 }}>
+          <Button title={"Reset"} onPress={()=>{
+              setCode({type:"", data:""});
+          }} />
+        </View>
+        <View style={{ padding: 10 }}>
+          <Button title={"Copy"} onPress={()=>{
+              Clipboard.setString(code.data)
+          }} />
+        </View>
+      </View>
+
+      <TextInput 
+        multiline={true}
+        editable={false}
+        numberOfLines={20}
+        style={{
+          backgroundColor: 'lightblue',
+          width: 400
+        }}
+        value={code.data}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +90,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  barcodebox: {
+    backgroundColor: 'tomato',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 400,
+    height: 300,
+    overflow: 'hidden',
+    borderRadius: 30
+  },
+  maintext: {
+    fontSize: 20,
+    margin: 20
+  }
 });
